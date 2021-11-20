1d225dc5e41eb677e06ae783c56ff4ed0a11ea1b
Dominik Piekarczyk <piekarczyk@gmail.com>
2021-11-20 21:33:44 +0100

    Add get exercises, get one, create exercise; store in memory

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from typing import Optional
+
+from fastapi import FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -6,7 +8,20 @@ app = FastAPI()
 
 class Exercise(BaseModel):
     name: str
-    description: str
+    description: Optional[str]
+
+
+stored_exercises = [
+    {"id": 1, "name": "The best exercise ever", "description": "Here's why: ..."},
+    {"id": 2, "name": "Second best exercise", "description": "Just after the first one..."},
+]
+
+
+def find_exercise(id):
+    for exe in stored_exercises:
+        if exe["id"] == int(id):
+            return exe
+    return None
 
 
 @app.get("/")
@@ -16,12 +31,26 @@ async def root():
 
 @app.get("/exercises")
 async def get_exercises():
-    return {"Exercises": f""}
+    return {"data": stored_exercises}
 
 
-@app.post("/exercise")
+@app.get("/exercises/{id}")
+async def get_exercise(id: int):
+    exe = find_exercise(id)
+    if not exe:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"ID {id} not found")
+    return exe
+
+
+@app.post("/exercise", status_code=status.HTTP_201_CREATED)
 async def post_exercise(exercise: Exercise):
-    return {"Exercise": f"{exercise}"}
+    # return {"Exercise": f"{exercise}"}
+    next_id = max([exe["id"] for exe in stored_exercises]) + 1
+    stored_exercises.append({"id": next_id,
+                             "name": exercise.name,
+                             "description": exercise.description})
+    return exercise.dict()
 
 
 # /exercises
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,5 @@
+import json
+
 from starlette.testclient import TestClient
 
 from main import app
@@ -11,9 +13,31 @@ def test_root():
     assert response.json()["message"] == "Hello World"
 
 
-def test_say_hello() -> None:
-    """Get a hello for someone"""
-    response = client.get("/hello/Jacek")
+def test_get_one_exe() -> None:
+    """Get one exercise"""
+    response = client.get("/exercises/1")
     result = response.json()
     assert response.status_code == 200
-    assert result["message"] == "Hello Jacek"
+    assert result['name'] == "The best exercise ever"
+
+
+def test_get_nonexisting_exe() -> None:
+    """Get an exe that does not exist; expect 404"""
+    response = client.get("/exercises/0")
+    result = response.json()
+    assert response.status_code == 404
+    assert 'detail' in result
+    assert 'not found' in result['detail']
+
+
+
+def test_post_exercise() -> None:
+    """Get a hello for someone"""
+    exe = {
+        "name": "Excercise number {{$randomInt}}",
+        "description": "Description: {{$randomCatchPhraseAdjective}} {{$randomCatchPhrase}}"
+    }
+    response = client.post('/exercise', data=json.dumps(exe))
+    result = response.json()
+    assert response.status_code == 201
+    assert "name" in result
